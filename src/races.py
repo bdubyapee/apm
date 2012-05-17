e6ed2e7785c989c7bb8d16a32762a67aa9cefe73
bdubyapee <phippsb@gmail.com>
2012-05-16 19:07:05 -0600

    Updated License file and added license information to source files.

--- src/races.py
+++ src/races.py
@@ -1,10 +1,26 @@
 #! usr/bin/env python
-# Project: Another Python MUD (apm)
+# 
+#  APM - Another Python Mud
+#  Copyright (C) 2012  bdubyapee (BWP)
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
 # Filename: races.py
 # 
 # File Description: Module to deal with races.
-# 
-# By: admin
+
 
 import os
 import glob
@@ -36,8 +52,8 @@ wearlocationslist = ('head', 'face', 'eyes', 'neck', 'left arm', 'right arm',
 
 height_list_values = [number for number in range(12)]
 ac_and_resistance_values = [number for number in range(-50, 51)]
-start_locationlist = {'drowhome': 101,  'stormhaven': 101, 'whitestone keep':101}
-special_skillslist = ['drow', 'elven', 'high elven', 'common']
+start_locationlist = {'default': 101}
+special_skillslist = ['common']
 
 class oneRace(olc.Editable):
     def __init__(self, path):
